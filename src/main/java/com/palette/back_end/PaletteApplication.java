a9bc058c0d77633f6eeda47c46fae221251190c9
koreanMike513 <paulryu1998@gmail.com>
2024-01-26 00:56:12 +0000

    init: installed packages and set env variables

---
+++ src/main/java/com/palette/back_end/PaletteApplication.java
@@ -0,0 +1,13 @@
+package com.palette.back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaletteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/palette/back_end/config/JPAConfig.java
@@ -0,0 +1,9 @@
+package com.palette.back_end.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JPAConfig {
+}
